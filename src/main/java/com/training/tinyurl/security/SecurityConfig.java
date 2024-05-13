ba7ebbadbd53dfe355184029ed23278c16619370
Lakshman1x <lkumar3@opentext.com>
2024-05-13 18:04:03 +0530

    comments fixed

--- src/main/java/com/training/tinyurl/security/SecurityConfig.java
+++ src/main/java/com/training/tinyurl/security/SecurityConfig.java
@@ -3,11 +3,9 @@ package com.training.tinyurl.security;
 import com.training.tinyurl.security.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -49,9 +47,4 @@ public class SecurityConfig {
         return authenticationProvider;
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
 }
